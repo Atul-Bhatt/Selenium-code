2f542c54cf0ed3d8e5cefe5df07733593ac1bfb5
Atul-Bhatt <atulbhatt61@gmail.com>
2021-04-12 20:39:49 +0530

    documentation

---
+++ Documentation.md
@@ -0,0 +1,7 @@
+# Using Selenium
+
+- Import selenium webdriver
+- Open the browser using driver.get(url)
+- Print the title using driver.title
+- To quit the browser use driver.quit()
+- To quit a single tab use driver.close()
--- selenium_tutorial.py
+++ selenium_tutorial.py
@@ -4,3 +4,7 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://medium.com")
+
+# Quit a tab with driver.close() if you want to close entire browser(all the tabs)
+# then use driver.quit()
+driver.close()
